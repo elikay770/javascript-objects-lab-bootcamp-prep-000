5363d63c24fa8aa041807a73b3b6dae3278fc857
Elliot Kahan <ejkahan@gmail.com>
2017-11-08 10:26:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
- 
+
   return object
   }
-
-
