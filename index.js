58f332388f340174caf28f14710202999abf680b
Elliot Kahan <ejkahan@gmail.com>
2017-11-08 11:00:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,9 +23,8 @@ function  deleteFromObjectByKey(object, key){
 return delete recipes.salad
 }
 
-const recipestwo = { omelette : 'eggs' , salad : 'lettuce'};
+var recipestwo = { omelette : 'eggs' , salad : 'lettuce'};
 function destructivelyDeleteFromObjectByKey(object, key) {
 
-return delete recipestwo.salad;
-recipestwo;
+return delete recipestwo.salad
 }
